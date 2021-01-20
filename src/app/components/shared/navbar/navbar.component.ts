bd8c9d23243010d0b79b3322fe7425e4faeee40c
Aditya <adityathakur532@gmail.com>
2021-01-21 03:52:28 +0530

    login based navbar

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { window } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  user = environment.currentUser;
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.setUser();
+  }
+  setUser() {
+    this.user = environment.currentUser;
+  }
+  logout() {
+    environment.currentUser = {
+      id: '',
+      fullName : '',
+      email : '',
+      gender : '',
+      dob : new Date(),
+      mobileNumber : ''
+    }
+    this.setUser();
+    this.router.navigateByUrl('/login');
   }
 
 }
